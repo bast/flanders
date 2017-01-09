af94e369d2d8bbea56c0e42f7484a49dcc4d8fdb
Radovan Bast <bast@users.noreply.github.com>
2017-01-09 17:02:26 +0100

    rm unused python code

--- flanders/distance.py
+++
@@ -1,3 +0,0 @@
-def get_distance(p1, p2):
-    from math import sqrt
-    return sqrt((p2[0] - p1[0])**2.0 + (p2[1] - p1[1])**2.0)
--- flanders/normalize.py
+++
@@ -1,9 +0,0 @@
-def normalize(v, s):
-    """
-    Normalize vector v to length s.
-    """
-    from math import sqrt
-
-    length = sqrt(v[0]**2 + v[1]**2)
-
-    return (s*v[0]/length, s*v[1]/length)
