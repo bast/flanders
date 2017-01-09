dc28ce6e06550c1a327fa56ad0914f5890c18312
Radovan Bast <bast@users.noreply.github.com>
2017-01-09 18:19:11 +0100

    make it possible to time module

--- test.py
+++ test.py
@@ -9,6 +9,7 @@ def test_library():
     y0 = x0
     y1 = x1
     num_points = 500
+    do_timing = False
 
     # generate random but reproducible data
     seed = 10
@@ -38,20 +39,26 @@ def test_library():
 
     # verify results without and with angles
     for use_angles in [False, True]:
+        t0 = time.time()
         for i, point in enumerate(points):
-            index_naive = cpp_interface.find_neighbor_naive(i,
-                                                            len(points),
-                                                            x_coordinates,
-                                                            y_coordinates,
-                                                            use_angles,
-                                                            [view_vectors[i][0], view_vectors[i][1]],
-                                                            view_angles[i])
+            if not do_timing:
+                index_naive = cpp_interface.find_neighbor_naive(i,
+                                                                len(points),
+                                                                x_coordinates,
+                                                                y_coordinates,
+                                                                use_angles,
+                                                                [view_vectors[i][0], view_vectors[i][1]],
+                                                                view_angles[i])
             index = cpp_interface.find_neighbor(context,
                                                 i,
                                                 use_angles,
                                                 [view_vectors[i][0], view_vectors[i][1]],
                                                 view_angles[i])
 
+        if do_timing:
+            print('\nuse_angles: {0}'.format(use_angles))
+            print('time used: {0}'.format(time.time() - t0))
+        else:
             assert index_naive == index
 
     cpp_interface.free_context(context)
