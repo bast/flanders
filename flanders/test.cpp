e59bc22de5a4a84f09d448fb8985b79cf573c6cc
Radovan Bast <bast@users.noreply.github.com>
2017-01-10 09:50:36 +0100

    add test code

---
+++ flanders/test.cpp
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "btree.h"
+
+
+int main()
+{
+    int num_points = 1000000;
+
+    double *x = new double[num_points];
+    double *y = new double[num_points];
+
+    srand(0);
+
+    for (int i = 0; i < num_points; i++)
+    {
+        x[i] = ((float) rand()) / (float) RAND_MAX;
+        y[i] = ((float) rand()) / (float) RAND_MAX;
+//      printf("x=%f y=%f\n", x[i], y[i]);
+    }
+
+    btree tree(num_points, x, y);
+
+    for (int i = 0; i < num_points; i++)
+    {
+        tree.insert(x[i], y[i], i);
+    }
+
+    delete[] x;
+    delete[] y;
+
+    return 0;
+}
