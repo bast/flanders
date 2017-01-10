c519f4bb6d227a9cd5636255e344ab05bc9d9d71
Radovan Bast <bast@users.noreply.github.com>
2017-01-10 10:24:55 +0100

    improve test to find mem leak

--- flanders/test.cpp
+++ flanders/test.cpp
@@ -6,7 +6,7 @@
 
 int main()
 {
-    int num_points = 1000000;
+    int num_points = 20;
 
     double *x = new double[num_points];
     double *y = new double[num_points];
@@ -17,10 +17,17 @@ int main()
     {
         x[i] = ((float) rand()) / (float) RAND_MAX;
         y[i] = ((float) rand()) / (float) RAND_MAX;
-//      printf("x=%f y=%f\n", x[i], y[i]);
     }
 
     btree tree(num_points, x, y);
+
+    for (int i = 0; i < num_points; i++)
+    {
+        double view_vector[2] = {1.0, 1.0};
+        double view_angle_deg = 20.0;
+        int index = tree.find_neighbor(i, true, view_vector, view_angle_deg);
+        printf("i=%i nearest=%i\n", i, index);
+    }
 
     delete[] x;
     delete[] y;
