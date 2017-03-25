49d17a8cce41268ba7638cec5ccc8632a6b9429b
Radovan Bast <bast@users.noreply.github.com>
2017-03-25 11:12:05 +0100

    pip install in release mode

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def run_cmake():
     os.chdir(_build_dir)
 
     try:
-        _spawn.spawn(['cmake', '..'])
+        _spawn.spawn(['cmake', '-DCMAKE_BUILD_TYPE=release', '..'])
     except _spawn.DistutilsExecError:
         print("Error while running CMake")
         sys.exit(-1)
