0c781459e562bab32408f8a0aeaf3fc8c1732f3b
Radovan Bast <bast@users.noreply.github.com>
2017-03-27 11:40:20 +0200

    refactor API

--- flanders/__init__.py
+++ flanders/__init__.py
@@ -26,15 +26,20 @@ _lib = get_lib_handle(
 # outward facing API
 
 def new_context(num_points,
-                x_coordinates,
-                y_coordinates):
+                points):
+
+    x_coordinates, y_coordinates = zip(*points)
 
     # cast a pointer which points to the numpy array data
     # we work with numpy because tree initialization with normal lists segfault
     # for lists longer than ca. 0.5 million points
     ffi = FFI()
-    x_coordinates_p = ffi.cast("double *", x_coordinates.ctypes.data)
-    y_coordinates_p = ffi.cast("double *", y_coordinates.ctypes.data)
+
+    x_coordinates_np = np.array(x_coordinates)
+    x_coordinates_p = ffi.cast("double *", x_coordinates_np.ctypes.data)
+
+    y_coordinates_np = np.array(y_coordinates)
+    y_coordinates_p = ffi.cast("double *", y_coordinates_np.ctypes.data)
 
     return _lib.new_context(num_points,
                             x_coordinates_p,
@@ -46,17 +51,16 @@ free_context = _lib.free_context
 
 def search_neighbor(context,
                     ref_indices=None,
-                    x=None,
-                    y=None,
-                    vx=None,
-                    vy=None,
+                    coordinates=None,
+                    view_vectors=None,
                     angles_deg=None,
                     naive=False):
 
     ffi = FFI()
 
     if ref_indices is None:
-        num_indices = len(x)
+        num_indices = len(coordinates)
+        x, y = zip(*coordinates)
         x_np = np.array(x)
         x_p = ffi.cast("double *", x_np.ctypes.data)
         y_np = np.array(y)
@@ -69,13 +73,14 @@ def search_neighbor(context,
     indices_np = np.zeros(num_indices, dtype=np.int32)
     indices_p = ffi.cast("int *", indices_np.ctypes.data)
 
-    if (vx is None and vy is None and angles_deg is None):
+    if (view_vectors is None and angles_deg is None):
         use_angles = False
         vx_p = ffi.new("double *")
         vy_p = ffi.new("double *")
         angles_deg_p = ffi.new("double *")
     else:
         use_angles = True
+        vx, vy = zip(*view_vectors)
         vx_np = np.array(vx)
         vx_p = ffi.cast("double *", vx_np.ctypes.data)
         vy_np = np.array(vy)
--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 import random
 from flanders import new_context, free_context, search_neighbor
-import numpy as np
 
 
 def test_library():
@@ -10,21 +9,16 @@ def test_library():
     assert num_points % num_batches == 0
     batch_length = int(num_points / num_batches)
 
-    x_coordinates = np.zeros(num_points, dtype=np.float64)
-    y_coordinates = np.zeros(num_points, dtype=np.float64)
-
     # generate random but reproducible data
     seed = 10
     random.seed(seed)
-    for i in range(num_points):
-        x_coordinates[i] = random.uniform(bounds[0], bounds[1])
-        y_coordinates[i] = random.uniform(bounds[0], bounds[1])
 
-    context = new_context(num_points, x_coordinates, y_coordinates)
-
-    vx = [random.uniform(bounds[0], bounds[1]) for _ in range(num_points)]
-    vy = [random.uniform(bounds[0], bounds[1]) for _ in range(num_points)]
+    points = [(random.uniform(bounds[0], bounds[1]), random.uniform(bounds[0], bounds[1])) for _ in range(num_points)]
+    view_vectors = [(random.uniform(bounds[0], bounds[1]), random.uniform(bounds[0], bounds[1])) for _ in range(num_points)]
     angles_deg = [random.uniform(10.0, 20.0) for _ in range(num_points)]
+    coordinates = [(random.uniform(bounds[0], bounds[1]), random.uniform(bounds[0], bounds[1])) for _ in range(num_points)]
+
+    context = new_context(num_points, points)
 
     # verify results without angles
     for batch in range(num_batches):
@@ -43,38 +37,26 @@ def test_library():
         i = batch*batch_length
         j = batch*batch_length + batch_length
         ref_indices = range(i, j)
-        batch_vx = vx[i:j]
-        batch_vy = vy[i:j]
-        batch_angles_deg = angles_deg[i:j]
         indices_fast = search_neighbor(context,
                                        ref_indices=ref_indices,
-                                       vx=batch_vx,
-                                       vy=batch_vy,
-                                       angles_deg=batch_angles_deg)
+                                       view_vectors=view_vectors[i:j],
+                                       angles_deg=angles_deg[i:j])
         indices_naive = search_neighbor(context,
                                         ref_indices=ref_indices,
-                                        vx=batch_vx,
-                                        vy=batch_vy,
-                                        angles_deg=batch_angles_deg,
+                                        view_vectors=view_vectors[i:j],
+                                        angles_deg=angles_deg[i:j],
                                         naive=True)
         assert indices_fast == indices_naive
 
-        # verify results with and free-style points
-        x = [random.uniform(bounds[0], bounds[1]) for _ in range(batch_length)]
-        y = [random.uniform(bounds[0], bounds[1]) for _ in range(batch_length)]
-
+        # verify results with free-style coordinates
         indices_fast = search_neighbor(context,
-                                       x=x,
-                                       y=y,
-                                       vx=batch_vx,
-                                       vy=batch_vy,
-                                       angles_deg=batch_angles_deg)
+                                       coordinates=coordinates[i:j],
+                                       view_vectors=view_vectors[i:j],
+                                       angles_deg=angles_deg[i:j])
         indices_naive = search_neighbor(context,
-                                        x=x,
-                                        y=y,
-                                        vx=batch_vx,
-                                        vy=batch_vy,
-                                        angles_deg=batch_angles_deg,
+                                        coordinates=coordinates[i:j],
+                                        view_vectors=view_vectors[i:j],
+                                        angles_deg=angles_deg[i:j],
                                         naive=True)
         assert indices_fast == indices_naive
 
