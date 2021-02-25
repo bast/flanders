5a628f7facab688600f03658b6275410bf5597b6
Radovan Bast <bast@users.noreply.github.com>
2021-02-25 11:01:07 +0100

    document the example

--- example/example.py
+++ example/example.py
@@ -1,5 +1,9 @@
 import flanders
 
+
+# as a first step we build the search tree
+# we can later reuse the search tree many times
+
 points = [
     (60.4, 51.3),
     (173.9, 143.8),
@@ -12,6 +16,9 @@ points = [
 tree = flanders.build_search_tree(points)
 
 
+# now we will search the indices of nearest neighbor points
+# for two observer points
+
 observer_coordinates = [(119.2, 59.7), (155.2, 30.2)]
 view_vectors = [(0.0, 1.0), (-1.0, -1.0)]
 view_angles_deg = [90.0, 90.0]
@@ -22,6 +29,10 @@ indices = flanders.nearest_indices_from_coordinates(
 
 assert indices == [2, -1]
 
+
+# instead of using observer coordinates, also the original
+# points themselves can be observers and we can select them
+# by their index
 
 observer_indices = [0, 1, 2, 3, 4, 5]
 view_vectors = [(1.0, 1.0) for _ in observer_indices]
